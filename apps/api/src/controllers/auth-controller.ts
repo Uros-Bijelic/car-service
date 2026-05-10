9a8e1e46733be8917c1eefdd19cd48e33dc49257
urke90 <urosbijelic90@gmail.com>
2026-05-10 13:08:29 +0200

    add db scripts, created database

--- apps/api/src/controllers/auth-controller.ts
+++ apps/api/src/controllers/auth-controller.ts
@@ -1,9 +1,34 @@
+import { hashPassword } from '@utils/password.js';
 import type { Response, Request } from 'express';
+import { db } from '@db/db.js';
+import { users } from '@db/schema.js';
 
 export const register = async (req: Request, res: Response) => {
     try {
-        // const { username, email, password } = req.body;
-        res.send('register');
+        const { username, email, password, firstName, lastName, phone } =
+            req.body;
+
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db
+            .insert(users)
+            .values({
+                username,
+                email,
+                password: hashedPassword,
+                firstName,
+                lastName,
+                phone
+            })
+            .returning({
+                username,
+                email,
+                firstName,
+                lastName,
+                phone
+            });
+
+        console.log('result', result);
     } catch (e) {
         console.error('Error on register', e);
     }
