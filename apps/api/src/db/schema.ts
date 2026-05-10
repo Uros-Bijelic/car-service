284848b7e04246f3ece11af51ef706415e84ed04
urke90 <urosbijelic90@gmail.com>
2026-05-10 10:27:14 +0200

    update schema with car vin

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -8,6 +8,7 @@ import {
     uuid,
     varchar
 } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
 export const appointmentStatusEnum = pgEnum('appointment_status', [
     'pending',
@@ -16,9 +17,9 @@ export const appointmentStatusEnum = pgEnum('appointment_status', [
     'cancelled'
 ]);
 
-export const usersTable = pgTable('users', {
+export const users = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
-    username: varchar('username', { length: 50 }).notNull(),
+    username: varchar('username', { length: 50 }).notNull().unique(),
     firstName: varchar('first_name', { length: 50 }),
     lastName: varchar('last_name', { length: 50 }),
     email: varchar('email', { length: 255 }).notNull().unique(),
@@ -28,35 +29,38 @@ export const usersTable = pgTable('users', {
     updatedAt: timestamp('updated_at').defaultNow().notNull()
 });
 
-export const carsTable = pgTable('cars', {
+export const cars = pgTable('cars', {
     id: uuid('id').primaryKey().defaultRandom(),
     userId: uuid('user_id')
-        .references(() => usersTable.id, {
+        .references(() => users.id, {
             onDelete: 'cascade'
         })
         .notNull(),
     make: varchar('make', { length: 255 }).notNull(),
     model: varchar('model', { length: 255 }).notNull(),
     year: integer().notNull(),
+    vin: varchar('vin', { length: 50 }).unique(),
+    licensePlate: varchar('license_plate', { length: 20 }).unique().notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull()
 });
 
-export const appointmentsTable = pgTable('appointments', {
+export const appointments = pgTable('appointments', {
     id: uuid('id').primaryKey().defaultRandom(),
     carId: uuid('car_id')
-        .references(() => carsTable.id, {
+        .references(() => cars.id, {
             onDelete: 'cascade'
         })
         .notNull(),
     description: text('description').notNull(),
     scheduledAt: timestamp('scheduled_at').notNull(),
     status: appointmentStatusEnum('status').default('pending').notNull(),
+    estimatedDurationMinutes: integer('estimated_duration_minutes'),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull()
 });
 
-export const servicesTable = pgTable('services', {
+export const services = pgTable('services', {
     id: uuid('id').primaryKey().defaultRandom(),
     name: varchar('name', { length: 255 }).notNull(),
     price: numeric('price').notNull(),
@@ -65,16 +69,57 @@ export const servicesTable = pgTable('services', {
     updatedAt: timestamp('updated_at').defaultNow().notNull()
 });
 
-export const appointmentsServicesTable = pgTable('appointment_services', {
+export const appointmentServices = pgTable('appointment_services', {
     id: uuid('id').primaryKey().defaultRandom(),
     appointmentId: uuid('appointment_id')
-        .references(() => appointmentsTable.id, { onDelete: 'cascade' })
+        .references(() => appointments.id, { onDelete: 'cascade' })
         .notNull(),
     serviceId: uuid('service_id')
-        .references(() => servicesTable.id, {
+        .references(() => services.id, {
             onDelete: 'restrict'
         })
         .notNull(),
     priceAtTime: numeric('price_at_time').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull()
 });
+
+export const userRelations = relations(users, ({ many }) => ({
+    cars: many(cars)
+}));
+
+export const carsRelations = relations(cars, ({ one, many }) => ({
+    user: one(users, {
+        fields: [cars.userId],
+        references: [users.id]
+    }),
+    appointments: many(appointments)
+}));
+
+export const appointmentsRelations = relations(
+    appointments,
+    ({ one, many }) => ({
+        car: one(cars, {
+            fields: [appointments.carId],
+            references: [cars.id]
+        }),
+        appointmentServices: many(appointmentServices)
+    })
+);
+
+export const servicesRelations = relations(services, ({ many }) => ({
+    appointmentServices: many(appointmentServices)
+}));
+
+export const appointmentServiceRelations = relations(
+    appointmentServices,
+    ({ one }) => ({
+        appointment: one(appointments, {
+            fields: [appointmentServices.appointmentId],
+            references: [appointments.id]
+        }),
+        service: one(services, {
+            fields: [appointmentServices.serviceId],
+            references: [services.id]
+        })
+    })
+);
