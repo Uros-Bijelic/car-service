051e4728273c2130eb07e56ad1f163aa93f0b34a
urke90 <urosbijelic90@gmail.com>
2026-05-14 14:22:31 +0200

    create UI and add rhf for register page. WIP - add fetch call update inputs to support email, password...

--- apps/web/src/pages/Register.tsx
+++ apps/web/src/pages/Register.tsx
@@ -1,17 +1,40 @@
 import RHFInput from '@/components/RHFComponents/RHFInput';
-import { FormProvider, useForm } from 'react-hook-form';
+import { FormProvider, useForm, type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod/v4';
+import { Button } from '@/components/ui/button';
 
-type Props = {};
+const registerSchema = z.object({
+    username: z.string().min(3, 'Username must be at least 3 characters long'),
+    email: z.email('Invalid email!'),
+    password: z.string().min(6, 'Password must be at least 6 characters long!')
+});
 
-export default function Register({}: Props) {
-    const methods = useForm();
+type RegisterSchema = z.infer<typeof registerSchema>;
+
+export default function Register() {
+    const methods = useForm<RegisterSchema>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            username: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<RegisterSchema> = (data) => {
+        console.log('data', data);
+    };
 
     return (
         <section className=" h-dvh flex-center">
             <div className="border border-blue-600 sm:w-sm max-w-2xl">
                 <h2 className="text-center mb-4">Register</h2>
                 <FormProvider {...methods}>
-                    <form action="" className="space-y-3 p-4 w-full">
+                    <form
+                        onSubmit={methods.handleSubmit(onSubmit)}
+                        className="space-y-3 p-4 w-full"
+                    >
                         <RHFInput
                             label="Username"
                             name="username"
@@ -27,6 +50,9 @@ export default function Register({}: Props) {
                             name="password"
                             placeholder="Password"
                         />
+                        <div className="flex-center">
+                            <Button type="submit">Register</Button>
+                        </div>
                     </form>
                 </FormProvider>
             </div>
